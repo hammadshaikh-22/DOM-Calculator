f27d6e34438e03052de01df7f68b46b7e49056af
hammadshaikh-22 <hammadshaikh1h23@gmail.com>
2025-11-27 01:09:18 +0500

    o

--- app.js
+++ app.js
@@ -16,18 +16,18 @@ output.value = ""
 // function clickFive(){
 //     output.value += "5"
 // }
-// function clickFour(){
-//     output.value += "4"
-// }
-// function clickThree(){
-//     output.value += "3"
-// }
-// function clickTwo(){
-//     output.value += "2"
-// }
-// function clickOne(){
-//     output.value += "1"
-// }
+function clickFour(){
+    output.value += "4"
+}
+function clickThree(){
+    output.value += "3"
+}
+function clickTwo(){
+    output.value += "2"
+}
+function clickOne(){
+    output.value += "1"
+}
 function clickZero(){
     output.value += "0"
 }
